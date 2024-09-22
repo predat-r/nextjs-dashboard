f3fedbbbefc073b28fdfa3daa71cd47c073694d9
predat-r <harisnaeem612@outlook.com>
2024-09-22 15:20:48 +0500

    first nextjs project

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerPage = () => {
+  return (
+    <p>CustomerPage</p>
+  )
+}
+
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvoicesPage = () => {
+  return (
+    <p>InvoicesPage</p>
+  )
+}
+
+export default InvoicesPage
